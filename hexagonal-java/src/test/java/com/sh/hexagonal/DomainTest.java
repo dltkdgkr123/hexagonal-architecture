a780700397da547b045a7a1bd7d4b3c232cc2cc6
이상학 <65546861+dltkdgkr123@users.noreply.github.com>
2024-12-10 11:30:07 +0900

    [test] junit5 dependency and simple connection test

---
+++ hexagonal-java/src/test/java/com/sh/hexagonal/DomainTest.java
@@ -0,0 +1,11 @@
+package com.sh.hexagonal;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class DomainTest {
+    @Test
+    void dependencyInjectionSmokeTest() {
+        Assertions.assertTrue(true);
+    }
+}
